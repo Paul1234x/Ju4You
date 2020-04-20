4768e13701768278b7cf7ba76b784e4618451593
minimangolattella <lukas-meinhard.koller@brg-judenburg.ac.at>
2020-04-20 14:36:08 +0200

    hallo

--- app/src/main/java/com/example/pauliistcool/MainActivity.java
+++ app/src/main/java/com/example/pauliistcool/MainActivity.java
@@ -5,12 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
+
 
     private Button bNachhilfe;
     private Button bStundenplan;
@@ -18,39 +18,74 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageButton ibSchullogo;
 
 
-    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://urania.webuntis.com/WebUntis/?school=brg-judenburg#/basic/main"));
-    Intent homepage = new Intent(Intent.ACTION_VIEW, Uri.parse(""));
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.bNachhilfe = this.findViewById(R.id.bNachhilfe);
-        this.bStundenplan = this.findViewById(R.id.bStundenplan);
-        this.bTexteBeispiele = this.findViewById(R.id.bTexteBeispiele);
-        this.ibSchullogo = this.findViewById(R.id.ibSchullogo);
 
-        this.bStundenplan.setOnClickListener((View.OnClickListener) this);
-        this.bNachhilfe.setOnClickListener((View.OnClickListener) this);
-        this.bTexteBeispiele.setOnClickListener((View.OnClickListener) this);
-        this.ibSchullogo.setOnClickListener((View.OnClickListener) this);
+
+
+        this.bNachhilfe = this.findViewById(R.id.bNachhilfe);
+        this.bNachhilfe = (Button) this.findViewById(R.id.bNachhilfe);
+        bNachhilfe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFaecher();
+            }
+
+        });
+
+        this.bStundenplan = this.findViewById(R.id.bStundenplan);
+        this.bStundenplan = (Button) this.findViewById(R.id.bStundenplan);
+        bStundenplan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openStundenplan();
+            }
+        });
+        this.bTexteBeispiele = this.findViewById(R.id.bTexteBeispiele);
+        this.bTexteBeispiele = (Button) this.findViewById(R.id.bTexteBeispiele);
+        bTexteBeispiele.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openTexteBeispiele();
+            }
+        });
+        this.ibSchullogo = this.findViewById(R.id.ibSchullogo);
+        this.ibSchullogo = (ImageButton) this.findViewById(R.id.ibSchullogo);
+        ibSchullogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSchullogo();
+            }
+        });
     }
 
+    private void openSchullogo() {
+        Uri uri = Uri.parse("https://www.brg-judenburg.ac.at/");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    private void openTexteBeispiele() {
+        Intent intent = new Intent(this, TexteBeispiele.class);
+        startActivity(intent);
+    }
+
+    private void openStundenplan() {
+        Intent intent = new Intent(this, Stundenplan.class);
+        startActivity(intent);
+    }
+
+    private void openFaecher() {
+        Intent intent = new Intent(this, Faecher.class);
+        startActivity(intent);
+    }
 
     @Override
     public void onClick(View view) {
-        Button clicked = (Button) view;
-        if(clicked.getId() == this.bStundenplan.getId()) {
-            startActivity(intent);
-        }
-        if(clicked.getId() == this.bTexteBeispiele.getId()) {
-            startActivity(wissmanont);
-        }
-        if(clicked.getId() == this.bNachhilfe.getId()) {
-            startActivity(Emifragen);
-        }
-        if(clicked.getId() == this.ibSchullogo.getId())  {
-            startActivity(homepage);
+
     }
-    }
+}
 
